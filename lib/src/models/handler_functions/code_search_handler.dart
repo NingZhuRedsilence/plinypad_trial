3b630453d82fdbfa1a6ddb82b3c537926795fa91
iamzhusir312@gmail.com <github551121>
2016-08-15 21:20:31 -0500

    added loading example code

--- lib/src/models/handler_functions/code_search_handler.dart
+++ lib/src/models/handler_functions/code_search_handler.dart
@@ -3,10 +3,10 @@ part of pad_models;
 class CodeSearchHandler
 {
   DefaultIdeController _controller;
+  //Todo: ?? make it abstract class here but also can use the getPrimarySourceFileContent method
   String _code = "";
   String _destination = "";
   Communicator _connection;
-  String _answerFromPliny = "";
 
   CodeSearchHandler(DefaultIdeController controller, String destination){
     _controller = controller;
@@ -17,7 +17,10 @@ class CodeSearchHandler
   dynamic call () async  {
     _code = _controller.getPrimarySourceFileContent();
     print("in CodeSearchHandler call(): _code = " + _code);
-    await _connection.send(_code);
+    await _connection.send(_code); // https://www.dartlang.org/tutorials/language/futures
+    // according to Dart doc, the next function won't execute until this line
+    // finishes, but _connection.message will still be null if the http_communicator
+    // doesn't use await
     await print("in CodeSearchHandler call(): answerFromPliny = " + _connection.message);
     await _controller.appendTextLineToConsole(_connection.message);
   }
